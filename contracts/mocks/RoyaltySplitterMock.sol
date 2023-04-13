466a946560fef7bb351d623dde8a9761e105902e
yuk6ra <yuki.imamura.all@gmail.com>
2023-04-13 18:56:26 +0900

    Update RoyaltySplitterMock

--- contracts/mocks/RoyaltySplitterMock.sol
+++ contracts/mocks/RoyaltySplitterMock.sol
@@ -8,17 +8,32 @@ import "./LicenseManagerMock.sol";
 contract RoyaltySplitterMock {
 
     CopyrightRegistryMock public copyrightRegistry;
+    LicenseManagerMock public licenseManager;
 
     constructor(
-        address _copyrightRegistryAddress
+        address _copyrightRegistryAddress,
+        address _licenseManagerAddress
     ) {
         copyrightRegistry = CopyrightRegistryMock(_copyrightRegistryAddress);
+        licenseManager = LicenseManagerMock(_licenseManagerAddress);
     }
 
     receive() external payable {}
 
-    function split() external payable {
-        
+    function split(
+        bytes32 _copyrightId
+    ) external payable {
+        (address[] memory authors, uint256[] memory shares) = copyrightRegistry.getAuthors(_copyrightId);
+
+        bytes32[] memory licenseIds = licenseManager.getLicenseIdsByCopyrightId(_copyrightId)
+
+        uint256 totalShares = 0;
+        for (uint256 i = 0; i < shares.length; i++) {
+            totalShares += shares[i];
+        }
+        for (uint256 i = 0; i < authors.length; i++) {
+            payable(authors[i]).transfer(msg.value * shares[i] / totalShares);
+        }
     }
 
     /// @dev modifier for only authors, but everyone can call split()
