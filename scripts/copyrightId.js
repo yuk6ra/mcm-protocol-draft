de3c383aa0f33a38298cbaf1089556aafa1c62ca
yuk6ra <yuki.imamura.all@gmail.com>
2023-04-13 17:33:40 +0900

    Add generate-id.js

--- scripts/copyrightId.js
+++
@@ -1,7 +0,0 @@
-const { ethers } = require("hardhat");
-
-const id = ethers.utils.solidityKeccak256(
-    ["uint256", "bytes32"],
-    ["1", "0x9faaf2a2acbef387565796dff70e992dc4474d774b03b04c352ca5ba329bcabe"]
-)
-console.log(id)
---
+++ scripts/generate-id.js
@@ -0,0 +1,13 @@
+const { ethers } = require("hardhat");
+
+const copyrightId = ethers.utils.solidityKeccak256(
+    ["uint256"],
+    [0]
+)
+console.log("Copyright ID: ", copyrightId);
+
+const licenseId = ethers.utils.solidityKeccak256(
+    ["uint256", "bytes32"],
+    [0, copyrightId]
+)
+console.log("License ID: ", licenseId);
