e50260d02a59d565936b51dbb705a407b8045d0b
yuk6ra <yuki.imamura.all@gmail.com>
2023-04-13 18:47:31 +0900

    Add RoyaltySplitterMock

--- contracts/mocks/RoyaltySplitterMock.sol
+++ contracts/mocks/RoyaltySplitterMock.sol
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+/// @dev must change to interfaces
+import "./CopyrightRegistryMock.sol";
+import "./LicenseManagerMock.sol";
+
 contract RoyaltySplitterMock {
 
+    CopyrightRegistryMock public copyrightRegistry;
+
+    constructor(
+        address _copyrightRegistryAddress
+    ) {
+        copyrightRegistry = CopyrightRegistryMock(_copyrightRegistryAddress);
+    }
+
+    receive() external payable {}
+
+    function split() external payable {
+        
+    }
+
+    /// @dev modifier for only authors, but everyone can call split()
+    // modifier onlyAutors(bytes32 _copyrightId) {
+    //     (address[] memory authors,) = copyrightRegistry.getAuthors(_copyrightId);
+    //     require();
+    //     _;
+    // }    
+    
 }
