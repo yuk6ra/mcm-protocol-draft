fc262cfd05ca265fb920de9439e1025cabc6b994
yuk6ra <yuki.imamura.all@gmail.com>
2023-04-17 17:00:04 +0900

    Add MCMDraft

---
+++ contracts/MCMDraft.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IMCMDraft.sol";
+
+contract MCMdraft is IMCMDraft, Ownable {
+    address public copyrightRegistryAddress;
+    address public licenseManagerAddress;
+    address public royaltySplitterAddress;
+
+    function setContractAddress(
+        address _copyrightRegistryAddress,
+        address _licenseManagerAddress,
+        address _royaltySplitterAddress
+    ) external onlyOwner {
+        copyrightRegistryAddress = _copyrightRegistryAddress;
+        licenseManagerAddress = _licenseManagerAddress;
+        royaltySplitterAddress = _royaltySplitterAddress;
+    }
+
+    function getContractAddress()
+        external
+        pure
+        returns (
+            address _copyrightRegistryAddress,
+            address _licenseManagerAddress,
+            address _royaltySplitterAddress
+        )
+    {
+        return (
+            _copyrightRegistryAddress,
+            _licenseManagerAddress,
+            _royaltySplitterAddress
+        );
+    }
+}
