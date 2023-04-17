e1f5da0b3e7aced5b377146c085e501096be7d10
yuk6ra <yuki.imamura.all@gmail.com>
2023-04-17 20:10:53 +0900

    Update

--- scripts/generate-id.js
+++ scripts/generate-id.js
@@ -1,13 +1,22 @@
 const { ethers } = require("hardhat");
 
-const copyrightId = ethers.utils.solidityKeccak256(
-    ["uint256"],
-    [2]
-)
-console.log("Copyright ID: ", copyrightId);
 
-const licenseId = ethers.utils.solidityKeccak256(
-    ["uint256", "bytes32"],
-    [0, copyrightId]
-)
-console.log("License ID: ", licenseId);
+for (let i = 0; i < 10; i++) {
+    console.log("=====", i, "=====")
+
+    let copyrightId = ethers.utils.solidityKeccak256(
+        ["uint256"],
+        [i]
+    )
+    console.log("Copyright ID: ", copyrightId);
+    console.log("baseURI:", `https://metadata.prtcl.xyz/metadata/copyrights/${i}.json`, "\n")
+
+    for (let j = 0; j < 5; j++) {    
+        let licenseId = ethers.utils.solidityKeccak256(
+            ["uint256", "bytes32"],
+            [j, copyrightId]
+        )
+        console.log(j,"License ID: ", licenseId);
+        console.log("baseURI:", `https://metadata.prtcl.xyz/metadata/licenses/copyright-${i}-license-${j}.json`, "\n")
+    }
+}
